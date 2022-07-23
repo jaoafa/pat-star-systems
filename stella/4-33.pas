d6413265f8910241ca8b6ce26dfd470575a907bf
Zakuro <34741088+lncongruence@users.noreply.github.com>
2022-07-23 09:07:16 +0900

    4'33"

---
+++ stella/4-33.pas
@@ -0,0 +1,42 @@
+{
+    "name": "4'33\"",
+    "description": "",
+    "creator": "Zakuro",
+    "version": "1.0.0",
+    "planets": [
+        {
+            "name": "",
+            "mass": 0,
+            "position_x": 0,
+            "position_y": 15800,
+            "velocity_x": -177.8920135498047,
+            "velocity_y": -0.000007775906851748005,
+            "required_thrust_to_move": 0,
+            "starting_planet": true,
+            "respawn": false,
+            "start_destroyed": true,
+            "min_spawn_delay": 0,
+            "max_spawn_delay": 0,
+            "planet": {
+                "seed": 0,
+                "radius": 0,
+                "heightRange": 50,
+                "waterHeight": 0,
+                "waterDepth": 100,
+                "temperature": 84,
+                "metalDensity": 0,
+                "metalClusters": 0,
+                "metalSpotLimit": -1,
+                "biomeScale": 50,
+                "biome": "gas",
+                "symmetryType": "none",
+                "symmetricalMetal": false,
+                "symmetricalStarts": false,
+                "numArmies": 1,
+                "landingZonesPerArmy": 0,
+                "landingZoneSize": 0,
+                "heightAdjustments": []
+            }
+        }
+    ]
+}
